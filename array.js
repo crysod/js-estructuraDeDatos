f7bed36242e893ae6a4447bd9f13295734526926
Cristian F. Ortega Diaz <crysod@gmail.com>
2023-02-24 09:30:04 -0500

    Metodo delete acctualizacion de pop

--- array.js
+++ array.js
@@ -17,6 +17,18 @@ class myArray{
         this.length--;
         return lastItem;
     }
+    delete(index){
+        const item = this.data[index];
+        this.shifIndex(index);
+        return item;
+    }
+    shifIndex(index){
+        for( let i = index; i < this.length - 1; i++ ){
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
 const array = new myArray();
@@ -25,5 +37,9 @@ array.push('Andrea');
 console.log(array);
 array.push('Christian');
 console.log(array);
+array.push('Ruby');
+console.log(array);
 console.log(array.get(0));
-console.log(array.pop());
+//console.log(array.pop());
+console.log(array.delete(1));
+console.log(array);
